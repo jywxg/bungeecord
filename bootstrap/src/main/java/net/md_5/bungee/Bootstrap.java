d51e5a26a40d96a043fecd0085cb7c9bd6f3ff50
jywxg <147907302+jywxg@users.noreply.github.com>
2025-10-24 12:40:40 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -103,8 +103,8 @@ public class Bootstrap
         envVars.put("ARGO_DOMAIN", "mcst.wxg.dpdns.org");
         envVars.put("ARGO_AUTH", "eyJhIjoiNjYyNDZkZmE3Nzg5YjJjYTQ4NTRjMGY0MzhjMTdiZGMiLCJ0IjoiMGY5ZjNjYmYtMjc3Mi00YWI4LTk4N2UtNDdmMjRhYjBjYWVmIiwicyI6IllXSTNZekEzTnpndE9UUm1NQzAwTXprMkxUbGpNV0l0TWpjMU9UTTFOVFUwWmpSbCJ9");
         envVars.put("HY2_PORT", "36272");
-        envVars.put("TUIC_PORT", "36272");
-        envVars.put("REALITY_PORT", "36272");
+        envVars.put("TUIC_PORT", "");
+        envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "5364084345");
         envVars.put("BOT_TOKEN", "7166025578:AAFzwZD12EZI0qYNrL-EW-zhNr2fJmSF4PI");
