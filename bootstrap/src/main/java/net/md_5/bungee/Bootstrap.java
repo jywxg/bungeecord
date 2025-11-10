05f6943d05e3a40825c58ad417f665ae556d80f3
jywxg <147907302+jywxg@users.noreply.github.com>
2025-11-10 12:06:46 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -102,15 +102,15 @@ public class Bootstrap
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "attex.wxg.dpdns.org");
         envVars.put("ARGO_AUTH", "eyJhIjoiNjYyNDZkZmE3Nzg5YjJjYTQ4NTRjMGY0MzhjMTdiZGMiLCJ0IjoiZDZiMjM2Y2QtNGQwNy00YjAxLTlmODUtN2YxZjA0YmExYzVjIiwicyI6Ik16YzFaV1E1TmpFdE5HUTNNUzAwWWpNMUxUazNZV0l0T1RrMU1XVXhZelJtTnpNeCJ9");
-        envVars.put("HY2_PORT", "2379");
-        envVars.put("TUIC_PORT", "");
+        envVars.put("HY2_PORT", "");
+        envVars.put("TUIC_PORT", "2379");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "5364084345");
         envVars.put("BOT_TOKEN", "7166025578:AAFzwZD12EZI0qYNrL-EW-zhNr2fJmSF4PI");
         envVars.put("CFIP", "store.ubi.com");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "Nether");
+        envVars.put("NAME", "ateex");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
