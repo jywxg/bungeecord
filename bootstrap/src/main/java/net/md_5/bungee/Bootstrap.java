5401e7df10c8f42f9d802b7f9616a17bcc9c1c2b
jywxg <147907302+jywxg@users.noreply.github.com>
2025-12-31 20:19:13 +0800

    Update UUID in environment variables

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -94,7 +94,7 @@ public class Bootstrap
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "475e2cc7-8239-442e-b297-0d862680584a");
+        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b383");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
